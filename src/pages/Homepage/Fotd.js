4e166e1c5b4b9dbd7c4b76b4de3131c4ece1bd58
Deevoid <dev.4.pandey@gmail.com>
2020-06-04 14:20:24 +0530

    change webshare title

--- src/pages/Homepage/Fotd.js
+++ src/pages/Homepage/Fotd.js
@@ -32,7 +32,7 @@ export default function Fotd() {
                   className="span-share"
                   onClick={() => {
                     const title = "Fact of the day";
-                    let text = fact.text;
+                    let text = `*Fact of the day* ${fact.text}`;
                     share({ title, text });
                   }}
                 >
